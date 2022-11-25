49c1f5df2de42ba3fb3deff7c640474eb96569a4
Jepc <joseenrique.pc21@gmail.com>
2022-11-25 06:16:45 -0400

    ADD stdin - stdout - Readline

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('colors');
 
+const {mostrarMenu, pausa}  = require('./helpers/messages');
+
 
 console.clear();
 
 const main = async() => {
   console.log('Hola Mundo');
 
-
+  mostrarMenu();
+  pausa();
 
 }
 
---
+++ helpers/messages.js
@@ -0,0 +1,48 @@
+require('colors');
+
+
+const mostrarMenu = () => {
+
+  console.clear();
+  console.log('============================='.green);
+  console.log('    Seleccione una opción'.green);
+  console.log('============================='.green);
+
+  console.log(`${'1.'.green} Crear tarea`);
+  console.log(`${'2.'.green} Listar tarea`);
+  console.log(`${'3.'.green} Listar tareas completadas`);
+  console.log(`${'4.'.green} Listar tareas pendientes`);
+  console.log(`${'5.'.green} Completar tarea(s)`);
+  console.log(`${'6.'.green} Borrar`);
+  console.log(`${'0.'.green} Salir`);
+
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('Selecciones una opción: ', (opt) => {
+    // console.log(opt);
+    readline.close();
+  })
+
+
+}
+
+const pausa = () => {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question(`\nSelecciones ${'ENTER'.green} para continuar\n`, (opt) => {
+    // console.log(opt);
+    readline.close();
+  })
+
+}
+
+module.exports = {
+  mostrarMenu,
+  pausa
+}
